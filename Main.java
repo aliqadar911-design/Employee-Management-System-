aceefc5d94656bc3812b0d66f5bef4f1263fd942
aliqadar911-design <ali.qadar911@gmail.com>
2026-07-19 01:47:24 +0500

    Add Main class with Swing GUI initialization

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+
+            MainFrame frame =
+                    new MainFrame();
+
+            frame.setVisible(true);
+        });
+    }
+}
